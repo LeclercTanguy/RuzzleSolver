4a6885f16a7a8a7f8c3fd0cb53fc1b51de250a94
Lardiere Nina <nlardiere@asi-testing>
2018-11-28 09:32:35 +0100

    modif Mot.h

--- Mot.h
+++ Mot.h
@@ -1,11 +1,11 @@
 #ifndef __Mot__
 #define __Mot__
 
-//typedef mot ;
+//typedef Mot ;
 
-Mot chaineEnMot (Dictionnaire dico, *char chaine) //precondition ??
-char motEnChaine (Mot lemot)
-unsigned int longueur (Mot lemot)
-void ajouterLettre(Mot lemot, char lettre, Dictionnaire dico) //precondition ??
-void retirerLettre(Mot lemot) //precondition ??
+Mot chaineEnMot (Dictionnaire dico, *char chaine); //precondition ??
+char motEnChaine (Mot lemot);
+unsigned int longueur (Mot lemot);
+void ajouterLettre(Mot lemot, char lettre, Dictionnaire dico); //precondition ??
+void retirerLettre(Mot lemot);//precondition ??
 #endif
