f16d4cc14d1181d217a71d816ce4b5c90007c8b1
Tanguy Leclerc <tanguy@MacBook-Pro-de-Tanguy.local>
2018-11-28 09:20:56 +0100

    Dictionnaire.h premier jet

--- include/Dictionnaire.h
+++ include/Dictionnaire.h
@@ -1,4 +1,11 @@
 #ifndef _Dictionnaire_
 #define _Dictionnaire_
 
-typedef 
+
+typedef Dictionnaire *Arbre;
+void ajouterMot(Dictionnaire , Mot);
+int estUnPrefixe(Dictionnaire,*char);
+int estUnMot(Dictionnaire,Mot);
+Ensemble obtenirLettresSuivantes(Dictionnaire,Mot);
+void sauvegarder(Dictionnaire);
+void charger(*char);
