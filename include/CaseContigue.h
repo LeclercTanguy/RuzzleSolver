3a2089b3a3aeb7d5c785f6c77fdf69f511d3ee75
Tanguy Leclerc <tanguy@MacBook-Pro-de-Tanguy.local>
2018-12-05 09:28:47 +0100

    les .h

--- include/CaseContigue.h
+++ include/CaseContigue.h
@@ -1,6 +1,7 @@
 #ifndef _CaseContigue_
 #define _CaseContigue_
 #include "Case.h"
+#include "ListeChainee.h"
 
 typedef struct CaseContigue{
     Case ListeChainee;
@@ -10,4 +11,4 @@ CaseContigue CaseContigue();
 void CC_ajouterCase(CaseContigue,Case);
 void CC_SuprimmerCase(CaseContigue);
 int CC_nbCaseContigue(CaseContigue);
-*char CC_CaseContigueEnChaine(CaseContigue); //Doit retourner une chaine de caractere 
+*char CC_CaseContigueEnChaine(CaseContigue); //Doit retourner une chaine de caractere
---
+++ include/ListeChainee.h
@@ -0,0 +1,5 @@
+#ifndef _ListeChainee_
+#define _ListeChainee_
+
+typedef struct LC_Noeud* LC_ListeChainee;
+typedef struct 
