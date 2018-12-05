f1abb0e44f2b9c37cafb687d9849075613e519b6
Lardiere Nina <nlardiere@asi-testing>
2018-12-05 08:56:01 +0100

    Mot.h

--- include/Mot.h
+++ include/Mot.h
@@ -1,11 +1,23 @@
 #ifndef __Mot__
 #define __Mot__
+#include "Dictionnaire.h"
 
-//typedef Mot ;
 
-Mot chaineEnMot (Dictionnaire dico, *char chaine); //precondition ??
+typedef Lettre* ListeChaineeDeLettres;
+typedef struct Lettre {
+  char caractere;
+  ListeChaineeDeLettres lettreSuivante;
+} Lettre;
+
+typedef struct Mot {
+  ListeChaineeDeLettres lettres;
+  int longueur;
+} Mot;
+
+
+Mot chaineEnMot (Dictionnaire dico, *char chaine);
 char motEnChaine (Mot lemot);
 unsigned int longueur (Mot lemot);
-void ajouterLettre(Mot lemot, char lettre, Dictionnaire dico); //precondition ??
-void retirerLettre(Mot lemot);//precondition ??
+void ajouterLettre(Mot lemot, char lettre, Dictionnaire dico);
+void retirerLettre(Mot lemot)
 #endif
