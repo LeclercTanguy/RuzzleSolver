1d1f40a5a6953988ca8e8340d41ed36e4e9a5603
yleguennec <yleguennec@insa-rouen.fr>
2018-12-09 00:13:44 +0100

    Dictionnaire.h reverted to previous version + repository cleaning

--- include/Dictionnaire.h
+++ include/Dictionnaire.h
@@ -1,14 +1,21 @@
-#ifndef _DICTIONNAIRE_
-#define _DICTIONNAIRE_
+#ifndef __DICTIONNAIRE__
+#define __DICTIONNAIRE__
+
+typedef struct Dictionnaire Dictionnaire;
+struct Dictionnaire{
+  char lettre;
+  Dictionnaire *lettreSuivante; // pointeur vers la première lettre dans l'ordre alphabétique pouvant remplacer la dernière lettre du préfixe
+  Dictionnaire *suffixe; //pointeur vers la première lettre pouvant être ajoutée au mot
+};
+
 #include "Mot.h"
 
-typedef Dictionnaire *Arbre;
-
-
-void ajouterMot(Dictionnaire , Mot);
-int estUnPrefixe(Dictionnaire,*char);
-int estUnMot(Dictionnaire,Mot);
-Ensemble obtenirLettresSuivantes(Dictionnaire,Mot);
-void sauvegarder(Dictionnaire);
-void charger(*char);
+Dictionnaire* creerDictionnaire(char *fichierDictionnaire);
+void ajouterMot(Dictionnaire* dico, char *leMot);
+void supprimerMot(Dictionnaire* dico, Mot motASupprimer);
+int estUnPrefixe(Dictionnaire dico, char *chaine);
+int estUnMot(Dictionnaire dico, Mot prefixe);
+Ensemble obtenirLettresSuivantes(Dictionnaire dico, Mot prefixe); //Ensemble de Caractères, à définir
+void sauvegarder(Dictionnaire dico, char *nomFichier);
+int charger(char *nomFichier, Dictionnaire *dico);
 #endif
