a32038596454129ab41a2dc377ecef138e88f316
yleguennec <yleguennec@insa-rouen.fr>
2018-12-24 00:28:34 +0100

    mots en majuscules

--- src/transcoder.c
+++ src/transcoder.c
@@ -3,6 +3,17 @@
 #include "Dictionnaire.h"
 #define LONGUEUR_MAX_MOT 30
 
+void chaineEnMajuscule(char* chaine) {
+  int i = 0;
+  int min2maj = 'A'-'a';
+  while (chaine[i]!='\0') {
+    if ((chaine[i]>='a') && (chaine[i]<='z')) {
+      chaine[i]=chaine[i]+min2maj;
+    }
+    i++;
+  }
+}
+
 int main(int argc, char* argv[]) {
   if (argc==3) {
     char* source = argv[1];
@@ -14,20 +25,21 @@ int main(int argc, char* argv[]) {
       char* err = fgets(mot,LONGUEUR_MAX_MOT,listeMots);
       while (err!=NULL) { // tant que l'on n'est pas à la fin du fichier
         *(mot+strlen(mot)-1)='\0'; // pour remplacer le \n provenant de la lecture du fichier
+        chaineEnMajuscule(mot); //pour que tous les mots soient en majuscule
         DC_ajouterMot (&dico,mot);
         err = fgets(mot,LONGUEUR_MAX_MOT,listeMots);
       }
       DC_sauvegarder(dico,destination);
       return fclose(listeMots);
     } else {
+      printf("Impossible d'ouvrir le fichier %s \n",source);
       return 1;
-      printf("Impossible d'ouvrir le fichier %s",source);
     }
   } else {
-    return 1;
     printf("Il n'y a pas assez d'arguments\n");
     printf("transcoder nécessite dans l'ordre : \n");
-    printf("1) le nom du fichier où est stocké le dictionnaire sous forme de liste de mots avec un mot par ligne");
-    printf("2) le nom du fichier où doit être enregistré le résultat de la sérialisation. Si le fichier existe, il sera écrasé.");
+    printf("1) le nom du fichier où est stocké le dictionnaire sous forme de liste de mots avec un mot par ligne. \n");
+    printf("2) le nom du fichier où doit être enregistré le résultat de la sérialisation. Si le fichier existe, il sera écrasé.\n");
+    return 1;
   }
 }
