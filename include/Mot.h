5f5dcba553a6a22119ffa003634c3cbb010c7fac
Simon Lebeaud <simon.lebeaud@insa-rouen.fr>
2018-12-28 15:25:02 +0100

    TAD Mot compile, encore les tests unitaires a faire

--- include/Mot.h
+++ include/Mot.h
@@ -2,25 +2,27 @@
 #define __MOT__
 #include "ListeChainee.h"
 
-typedef struct {
-  LC_ListeChainee *lettres; //Liste chaînée de Mot_Lettre, commençant par la dernière lettre du mot
-  unsigned int longueur;
+typedef struct Mot{
+  LC_ListeChainee lettres; //Liste chaînée de Mot_Lettre, commençant par la dernière lettre du mot
+  unsigned int taille;
 } Mot;
 
 #include "Dictionnaire.h"
 
-struct Mot_Lettre{
+typedef struct Mot_Lettre{
   char lettre;
   Dictionnaire refDico; //pointeur vers le nœud correspondant dans le dictionnaire
                         //à générer avec DC_obtenirReferenceLettre(refPrécédente,lettreÀAjouter)
-};
+}Mot_Lettre;
 
 Mot creerMot (void);
-char Mot_obtenirLettre(Mot leMot);
+unsigned int Mot_obtenirTaille(Mot leMot);
+LC_ListeChainee Mot_obtenirLesElements(Mot leMot);
+char Mot_obtenirLettre(Mot_Lettre motLettre);
+Mot* Mot_allouer(size_t taille);
 void Mot_ajouterLettre(Mot *leMot, char lettre, Dictionnaire *dico);
 void Mot_retirerLettre(Mot *leMot);
 Mot chaineEnMot (Dictionnaire *dico, char *chaine);
-char motEnChaine (Mot leMot);
-unsigned int Mot_longueur (Mot leMot);
+char* motEnChaine (Mot leMot);
 
 #endif
--- src/Mot.c
+++ src/Mot.c
@@ -7,14 +7,63 @@
 
 Mot Mot_creerMot(void){
   Mot leMot;
-  LeMot.lettres = *NULL;
-  LeMot.longueur = 0;
+  leMot.lettres = NULL;
+  leMot.taille = 0;
+  return leMot;
 }
 
-char Mot_obtenirLettre(Mot leMot){
-  return leMot.lettres.lettre;
+Mot* Mot_allouer(size_t taille) {
+  return (Mot*)LC_allouer(taille);
+}
+
+unsigned int Mot_obtenirTaille(Mot leMot){
+  return leMot.taille;
+}
+
+LC_ListeChainee Mot_obtenirLesElements(Mot leMot){
+  return leMot.lettres;
+}
+
+char Mot_obtenirLettre(Mot_Lettre motLettre){
+  return motLettre.lettre;
 }
 
 void Mot_ajouterLettre(Mot *leMot, char lettre, Dictionnaire *dico){
-  &leMot.lettres
+  Mot_Lettre motLettre;
+  LC_ListeChainee laListeDeLettre = Mot_obtenirLesElements(*leMot);
+  motLettre.lettre = lettre;
+  motLettre.refDico = DC_obtenirReferenceLettre(*dico,lettre);
+  LC_ajouter(&laListeDeLettre,&motLettre,Mot_obtenirTaille(*leMot));
+  leMot->taille=Mot_obtenirTaille(*leMot)+1;
+}
+
+void Mot_retirerLettre(Mot* leMot){
+  assert(!LC_estVide(Mot_obtenirLesElements(*leMot)));
+  leMot->taille = Mot_obtenirTaille(*leMot)-1;
+  LC_ListeChainee laListeDeLettre = Mot_obtenirLesElements(*leMot);
+  LC_supprimerTete(&laListeDeLettre);
+}
+
+char* motEnChaine(Mot leMot){
+  char* chaine[Mot_obtenirTaille(leMot)+1];
+  int i=0;
+  LC_ListeChainee laListeDeLettre = Mot_obtenirLesElements(leMot);
+  Mot_Lettre motLettre;
+  do{
+    motLettre = *(Mot_Lettre*)LC_obtenirElement(laListeDeLettre);
+    *chaine[i] = Mot_obtenirLettre(motLettre);
+    i++;
+    laListeDeLettre = LC_obtenirListeSuivante(laListeDeLettre);
+  }while(!LC_estVide(laListeDeLettre));
+  return *chaine;
+}
+
+Mot chaineEnMot(Dictionnaire *dico, char *chaine){
+  Mot *leMot = Mot_allouer(sizeof(chaine));
+  int i = 0;
+  do{
+    Mot_ajouterLettre(leMot,chaine[i],dico);
+    i++;
+  }while(chaine[i]!='\0');
+  return *leMot;
 }
