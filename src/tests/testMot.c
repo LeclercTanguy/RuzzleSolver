37a0bf6817674b9d29cd2fbd3ca66a8d48fbe885
yleguennec <yleguennec@insa-rouen.fr>
2018-12-31 18:44:54 +0100

    signalement d'erreur par retour de fonction sur ajouterLettre
    obtenirReferenceDictionnaire renommé en obtenirDerniereReferenceDictionnaire

--- src/tests/testMot.c
+++ src/tests/testMot.c
@@ -6,22 +6,22 @@
 
 void test_Mot_creerMot(void){
   Mot leMot = Mot_creerMot();
-  CU_ASSERT_TRUE((Mot_estVide(leMot))&& (Mot_obtenirTaille(leMot) ==0));
+  CU_ASSERT_TRUE((Mot_estVide(leMot))&& (Mot_obtenirTaille(leMot)==0));
 }
 
 void test_Mot_ajouterLettre(void){
   Dictionnaire dico = DC_creerDictionnaire();
-  Mot leMot;
+  Mot leMot = Mot_creerMot();
+  int err;
   DC_ajouterMot(&dico,"MOT");
-  Mot_ajouterLettre(&leMot,'T',dico);
-  Mot_ajouterLettre(&leMot,'O',dico);
-  Mot_ajouterLettre(&leMot,'M',dico);
-  Mot_Lettre motLettre = (*(Mot_Lettre*)LC_obtenirElement(leMot.lettres));
-  //char lettre1 = 't';
-  //char lettre2 = 'o';
-  char lettre3 = 'M';
-  CU_ASSERT_TRUE(( motLettre.lettre == lettre3) && (Mot_obtenirTaille(leMot)==1));
-
+  err = Mot_ajouterLettre(&leMot,'M',dico);
+  CU_ASSERT_FALSE(err);
+  err = Mot_ajouterLettre(&leMot,'O',dico);
+  CU_ASSERT_FALSE(err);
+  err = Mot_ajouterLettre(&leMot,'T',dico);
+  CU_ASSERT_FALSE(err);
+  Mot_Lettre derniereLettre = (*(Mot_Lettre*)LC_obtenirElement(Mot_obtenirLesElements(leMot)));
+  CU_ASSERT_TRUE((Mot_obtenirLettre(derniereLettre)=='T') && (Mot_obtenirTaille(leMot)==3));
 }
 
 
@@ -44,8 +44,6 @@ int main(int argc, char** argv){
   /* Ajout des tests à la suite de tests boite noire */
   if ((NULL == CU_add_test(pSuite, "créer un Mot Vide", test_Mot_creerMot))
       || (NULL == CU_add_test(pSuite, "ajouter une lettre au Mot", test_Mot_ajouterLettre))
-      //|| (NULL == CU_add_test(pSuite, "obtenir élément et liste suivante", test_LC_obtenir))
-      //|| (NULL == CU_add_test(pSuite,"fixer élément et liste suivante", test_LC_fixer))
       )
     {
       CU_cleanup_registry();
